64da51059f496b3f0a0fa238d2384a46165b5e8a
reynaldo0 <reynaldooyusellino@gmail.com>
2024-09-17 11:26:47 +0700

    add search

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,9 +1,12 @@
 import ContactsTable from "@/components/contacts-table"
+import Search from "@/components/search"
 
 const Contacts = () => {
   return (
-    <div className="max-w-screen-md mx-auto mt-5">
-        <div className="flex items-center justify-between gap-1 mb-5"></div>
+    <div className="max-w-screen-lg mx-auto mt-5">
+        <div className="flex items-center justify-between gap-1 mb-5">
+            <Search/>
+        </div>
         <ContactsTable/>
     </div>
   )

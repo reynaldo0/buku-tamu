dac4fec601e18955e0f4f6163e4a33fca8fa8ac2
reynaldo0 <reynaldooyusellino@gmail.com>
2024-09-17 19:45:37 +0700

    add default value update

--- src/components/edit-form.tsx
+++ src/components/edit-form.tsx
@@ -3,22 +3,24 @@
 import { saveContact } from "@/lib/action";
 import { useFormState } from "react-dom";
 import { SubmitButton } from "./buttons";
+import type { Contacts } from "@prisma/client";
 
-const UpdateForm = () => {
+const UpdateForm = ({ contact }: { contact: Contacts }) => {
     const [state, formAction] = useFormState(saveContact, null);
     return (
         <div>
             <form action={formAction}>
                 <div className="mb-5">
                     <label htmlFor="name" className="block text-sm font-medium text-gray-900">Nama Lengkap</label>
-                    <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Nama Lengkap" />
+                    <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Nama Lengkap"
+                        defaultValue={contact.name} />
                     <div id="name-error" aria-live="polite" aria-atomic="true">
                         <p className="mt-2 text-sm text-red-500">{state?.Error?.name}</p>
                     </div>
                 </div>
                 <div className="mb-5">
                     <label htmlFor="keperluan" className="block text-sm font-medium text-gray-900">Keperluan</label>
-                    <input type="text" name="keperluan" id="keperluan" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Keperluan Anda" />
+                    <input type="text" name="keperluan" id="keperluan" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Keperluan Anda" defaultValue={contact.keperluan} />
                     <div id="keperluan-error" aria-live="polite" aria-atomic="true">
                         <p className="mt-2 text-sm text-red-500">{state?.Error?.keperluan}</p>
                     </div>
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,9 +1,20 @@
 import { prisma } from "@/lib/prisma";
 
-export const getContacts = async () =>{
+export const getContacts = async () => {
     try {
         const contacts = await prisma.contacts.findMany();
         return contacts;
+    } catch (error) {
+        throw new Error("Failed to fetch contacts data")
+    }
+}
+
+export const getContactsById = async (id: string) => {
+    try {
+        const contact = await prisma.contacts.findUnique({
+            where: { id }
+        });
+        return contact;
     } catch (error) {
         throw new Error("Failed to fetch contacts data")
     }

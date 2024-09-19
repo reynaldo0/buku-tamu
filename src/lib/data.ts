69de83e841e14feaf7fe6e9831845d002195ba58
reynaldo0 <reynaldooyusellino@gmail.com>
2024-09-19 12:16:16 +0700

    tes

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,14 +1,12 @@
 import { prisma } from "@/lib/prisma";
-import { resolve } from "path";
-import { number, promise } from "zod";
 
 const ITEM_PER_PAGE = 5;
 
 export const getContacts = async (query: string, currentPage: number) => {
-    const offset = (currentPage - 1) * ITEM_PER_PAGE
+    const offset = (currentPage - 1) * ITEM_PER_PAGE;
 
     try {
-        await new Promise((resolve) => setTimeout(resolve, 2000));
+        await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulasi delay
         const contacts = await prisma.contacts.findMany({
             skip: offset,
             take: ITEM_PER_PAGE,
@@ -31,7 +29,7 @@ export const getContacts = async (query: string, currentPage: number) => {
         });
         return contacts;
     } catch (error) {
-        throw new Error("Failed to fetch contacts data")
+        throw new Error("Failed to fetch contacts data");
     }
 }
 
@@ -42,14 +40,13 @@ export const getContactsById = async (id: string) => {
         });
         return contact;
     } catch (error) {
-        throw new Error("Failed to fetch contacts data")
+        throw new Error("Failed to fetch contacts data");
     }
 }
 
 export const getContactsPages = async (query: string) => {
-
     try {
-        const contacts = await prisma.contacts.count({
+        const contactsCount = await prisma.contacts.count({
             where: {
                 OR: [
                     {
@@ -67,9 +64,9 @@ export const getContactsPages = async (query: string) => {
                 ]
             }
         });
-        const totalPages = Math.ceil(Number(contacts) / ITEM_PER_PAGE)
+        const totalPages = Math.ceil(contactsCount / ITEM_PER_PAGE);
         return totalPages;
     } catch (error) {
-        throw new Error("Failed to fetch contacts data")
+        throw new Error("Failed to fetch contacts data");
     }
 }
